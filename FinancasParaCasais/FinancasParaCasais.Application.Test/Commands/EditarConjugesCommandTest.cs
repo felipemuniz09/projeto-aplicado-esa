324feb96a2b81460fda90e7b46d4869c43f6c4df
felipemuniz09 <felipemuniz09@gmail.com>
2022-09-15 20:22:09 -0300

    Testes de unidade para o EditarConjugesCommand

--- FinancasParaCasais/FinancasParaCasais.Application.Test/Commands/EditarConjugesCommandTest.cs
+++ FinancasParaCasais/FinancasParaCasais.Application.Test/Commands/EditarConjugesCommandTest.cs
@@ -1,17 +1,78 @@
-﻿namespace FinancasParaCasais.Application.Test.Commands
+﻿using FinancasParaCasais.Application.Commands;
+using FluentAssertions;
+
+namespace FinancasParaCasais.Application.Test.Commands
 {
     public class EditarConjugesCommandTest
     {
         [Fact]
-        public void DeveConsiderarInvalidoQuandoListaDeConjugesForNull() { }
+        public void DeveConsiderarInvalidoQuandoListaDeConjugesForNull() 
+        {
+            // Given
+            var editarConjugesCommand = new EditarConjugesCommand();
+
+            // When
+            editarConjugesCommand.Validar();
+
+            // Then
+            editarConjugesCommand.Notifications.Should().Contain(n => n.Message.Equals("Lista de conjuges deve conter 2 elementos."));
+        }
 
         [Fact]
-        public void DeveConsiderarInvalidoQuandoListaDeConjugesForVazia() { }
+        public void DeveConsiderarInvalidoQuandoListaDeConjugesForVazia() 
+        {
+            // Given
+            var editarConjugesCommand = new EditarConjugesCommand
+            {
+                Conjuges = new List<EditarConjugesCommand.ConjugeCommand>()
+            };
+
+            // When
+            editarConjugesCommand.Validar();
+
+            // Then
+            editarConjugesCommand.Notifications.Should().Contain(n => n.Message.Equals("Lista de conjuges deve conter 2 elementos."));
+        }
 
         [Fact]
-        public void DeveConsiderarInvalidoQuandoListaDeConjugesNaoTiverExatamenteDoisElementos() { }
+        public void DeveConsiderarInvalidoQuandoListaDeConjugesNaoTiverExatamenteDoisElementos() 
+        {
+            // Given
+            var editarConjugesCommand = new EditarConjugesCommand
+            {
+                Conjuges = new List<EditarConjugesCommand.ConjugeCommand>
+                {
+                    new EditarConjugesCommand.ConjugeCommand(),
+                    new EditarConjugesCommand.ConjugeCommand(),
+                    new EditarConjugesCommand.ConjugeCommand()
+                }
+            };
+
+            // When
+            editarConjugesCommand.Validar();
+
+            // Then
+            editarConjugesCommand.Notifications.Should().Contain(n => n.Message.Equals("Lista de conjuges deve conter 2 elementos."));
+        }
 
         [Fact]
-        public void DeveConsiderarInvalidoQuandoSomatorioDosPercentuaisForDiferenteDeCem() { }
+        public void DeveConsiderarInvalidoQuandoSomatorioDosPercentuaisForDiferenteDeCem() 
+        {
+            // Given
+            var editarConjugesCommand = new EditarConjugesCommand
+            {
+                Conjuges = new List<EditarConjugesCommand.ConjugeCommand>
+                {
+                    new EditarConjugesCommand.ConjugeCommand { Percentual = 25 },
+                    new EditarConjugesCommand.ConjugeCommand { Percentual = 70 }
+                }
+            };
+
+            // When
+            editarConjugesCommand.Validar();
+
+            // Then
+            editarConjugesCommand.Notifications.Should().Contain(n => n.Message.Equals("Somatório dos percentuais deve ser igual a 100."));
+        }
     }
 }
