face195cc0767fec8de8643bdb01051a5ba889f0
felipe.muniz <felipe.muniz@animaeducacao.com.br>
2022-09-15 15:53:55 -0300

    implementação da consulta de conjuges

--- FinancasParaCasais/FinancasParaCasais.Application/QueryServices/ConjugeQueryService.cs
+++ FinancasParaCasais/FinancasParaCasais.Application/QueryServices/ConjugeQueryService.cs
@@ -1,13 +1,31 @@
-﻿using FinancasParaCasais.Application.DTOs;
+﻿using Dapper;
+using FinancasParaCasais.Application.DTOs;
 using FinancasParaCasais.Application.Interfaces.QueryServices;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace FinancasParaCasais.Application.QueryServices
 {
     public class ConjugeQueryService : IConjugeQueryService
     {
+        private readonly IConfiguration _configuration;
+
+        public ConjugeQueryService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IReadOnlyCollection<ConjugeDTO> ObterConjuges()
         {
-            return new List<ConjugeDTO> { new ConjugeDTO { Codigo = Guid.NewGuid(), Nome = "teste", Percentual = 70 } };
+            using var connection = new SqlConnection(_configuration.GetConnectionString("FinancasParaCasaisDB"));
+
+            const string sql = @"
+                SELECT  Codigo, Nome, Percentual
+                FROM    dbo.Conjuges";
+
+            var conjuges = connection.Query<ConjugeDTO>(sql);
+
+            return conjuges.ToList();
         }
     }
 }
