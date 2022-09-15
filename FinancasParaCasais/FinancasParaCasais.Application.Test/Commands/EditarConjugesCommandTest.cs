0ec62c6e19a6bd406dafdcac4a74837a442ab9e4
felipemuniz09 <felipemuniz09@gmail.com>
2022-09-15 19:29:02 -0300

    Cabeçalhos dos testes para EditarConjugesCommand

---
+++ FinancasParaCasais/FinancasParaCasais.Application.Test/Commands/EditarConjugesCommandTest.cs
@@ -0,0 +1,17 @@
+﻿namespace FinancasParaCasais.Application.Test.Commands
+{
+    public class EditarConjugesCommandTest
+    {
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoListaDeConjugesForNull() { }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoListaDeConjugesForVazia() { }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoListaDeConjugesNaoTiverExatamenteDoisElementos() { }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoSomatorioDosPercentuaisForDiferenteDeCem() { }
+    }
+}
