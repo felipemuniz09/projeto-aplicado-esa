a02e87477a1ee35b0b1511ab18e319f3adce0e54
felipe.muniz <felipe.muniz@animaeducacao.com.br>
2022-09-16 11:41:44 -0300

    casos de teste para entidade Conjuge

--- FinancasParaCasais/FinancasParaCasais.Domain.Test/Entities/ConjugeTest.cs
+++ FinancasParaCasais/FinancasParaCasais.Domain.Test/Entities/ConjugeTest.cs
@@ -2,5 +2,25 @@
 {
     public class ConjugeTest
     {
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoCodigoForVazio() { }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoNomeForNull() { }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoNomeForVazio() { }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoNomeForApenasEspacoEmBranco() { }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoNomeExcederTamanhoMaximo() { }
+
+        [Fact]
+        public void DeveConsiderarPercentualInvalidoQuandoForMenorQueZero() { }
+
+        [Fact]
+        public void DeveConsiderarPercentualInvalidoQuandoForMaiorQueCem() { }
     }
 }
