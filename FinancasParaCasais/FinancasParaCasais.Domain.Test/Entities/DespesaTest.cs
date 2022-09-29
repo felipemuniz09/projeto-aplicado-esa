bf60a3bbbe3327245c8e65f3935532f06e8f0967
felipemuniz09 <felipemuniz09@gmail.com>
2022-09-28 21:39:44 -0300

    implementação da entidade Despesa

---
+++ FinancasParaCasais/FinancasParaCasais.Domain.Test/Entities/DespesaTest.cs
@@ -0,0 +1,18 @@
+﻿using FinancasParaCasais.Domain.Entities;
+using FluentAssertions;
+
+namespace FinancasParaCasais.Domain.Test.Entities
+{
+    public class DespesaTest
+    {
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoCodigoForVazio()
+        {
+            // When
+            var despesa = new Despesa(Guid.Empty, "Almoço", 50);
+
+            // Then
+            despesa.Notifications.Should().Contain(n => n.Message == "Código deve ser informado.");
+        }
+    }
+}
---
+++ FinancasParaCasais/FinancasParaCasais.Domain/Entities/Despesa.cs
@@ -0,0 +1,19 @@
+﻿using Flunt.Validations;
+
+namespace FinancasParaCasais.Domain.Entities
+{
+    public class Despesa : BaseEntity
+    {
+        public string Descricao { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataHoraCriacao { get; private set; }
+
+        public Despesa(Guid codigo, string descricao, decimal valor) 
+            : base(codigo)
+        {
+            Descricao = descricao;
+            Valor = valor;
+            DataHoraCriacao = DateTime.Now;
+        }
+    }
+}
