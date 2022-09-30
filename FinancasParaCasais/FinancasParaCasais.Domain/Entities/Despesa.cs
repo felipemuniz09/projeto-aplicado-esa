2e99772a81c66e74ee5467cade326693dc0af1f5
felipemuniz09 <felipemuniz09@gmail.com>
2022-09-29 22:12:49 -0300

    - validação de valor maior/igual a zero; - inclusão dos pagamentos;

--- FinancasParaCasais/FinancasParaCasais.Domain/Entities/Despesa.cs
+++ FinancasParaCasais/FinancasParaCasais.Domain/Entities/Despesa.cs
@@ -1,9 +1,12 @@
-﻿using Flunt.Validations;
+﻿using FinancasParaCasais.Domain.ValueObject;
+using Flunt.Validations;
 
 namespace FinancasParaCasais.Domain.Entities
 {
     public class Despesa : BaseEntity
     {
+        private List<PagamentoDespesaValueObject> _pagamentos;
+
         public string Descricao { get; private set; }
         public decimal Valor { get; private set; }
         public DateTime DataHoraCriacao { get; private set; }
@@ -11,9 +14,24 @@ namespace FinancasParaCasais.Domain.Entities
         public Despesa(Guid codigo, string descricao, decimal valor) 
             : base(codigo)
         {
+            _pagamentos = new List<PagamentoDespesaValueObject>();
+
             Descricao = descricao;
             Valor = valor;
             DataHoraCriacao = DateTime.Now;
+
+            AddNotifications(new Contract<Despesa>()
+                .IsGreaterOrEqualsThan(Valor, 0, "Valor", "Valor deve ser maior ou igual a zero."));
+        }
+
+        public PagamentoDespesaValueObject AdicionarPagamento(Guid codigoConjuge, decimal valor)
+        {
+            var pagamentoDespesaValueObject = new PagamentoDespesaValueObject(codigoConjuge, valor);
+
+            if (pagamentoDespesaValueObject.IsValid)
+                _pagamentos.Add(pagamentoDespesaValueObject);
+
+            return pagamentoDespesaValueObject;
         }
     }
 }
