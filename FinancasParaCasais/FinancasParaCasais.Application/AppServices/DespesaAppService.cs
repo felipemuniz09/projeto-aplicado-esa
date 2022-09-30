57a94d7e0418ccccd27905044ebff331759ae4b0
felipe.muniz <felipe.muniz@animaeducacao.com.br>
2022-09-30 17:26:05 -0300

    implementação da camada Application para inserção de Despesa

---
+++ FinancasParaCasais/FinancasParaCasais.Application/AppServices/DespesaAppService.cs
@@ -0,0 +1,35 @@
+﻿using FinancasParaCasais.Application.Commands;
+using FinancasParaCasais.Application.Interfaces.AppServices;
+using FinancasParaCasais.Domain.Entities;
+using FinancasParaCasais.Domain.Interfaces.Services;
+
+namespace FinancasParaCasais.Application.AppServices
+{
+    public class DespesaAppService : IDespesaAppService
+    {
+        private readonly IDespesaService _despesaService;
+
+        public DespesaAppService(IDespesaService despesaService)
+        {
+            _despesaService = despesaService;
+        }
+
+        public void InserirDespesa(InserirDespesaCommand inserirDespesaCommand)
+        {
+            if (inserirDespesaCommand == null)
+                throw new ArgumentNullException(nameof(inserirDespesaCommand));
+
+            inserirDespesaCommand.Validar();
+
+            if (inserirDespesaCommand.IsValid)
+            {
+                var despesa = new Despesa(inserirDespesaCommand.Descricao, inserirDespesaCommand.Valor);
+
+                foreach (var pagamento in inserirDespesaCommand.Pagamentos)
+                    despesa.AdicionarPagamento(pagamento.CodigoConjuge, pagamento.Valor);
+
+                _despesaService.InserirDespesa(despesa);
+            }
+        }
+    }
+}
---
+++ FinancasParaCasais/FinancasParaCasais.Application/Commands/InserirDespesaCommand.cs
@@ -0,0 +1,31 @@
+﻿using Flunt.Notifications;
+
+namespace FinancasParaCasais.Application.Commands
+{
+    public class InserirDespesaCommand : Notifiable<Notification>
+    {
+        public string? Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public IReadOnlyCollection<PagamentoDespesaCommand>? Pagamentos { get; set; }
+
+        public class PagamentoDespesaCommand
+        {
+            public Guid CodigoConjuge { get; set; }
+            public decimal Valor { get; set; }
+        }
+
+        public void Validar()
+        {
+            if (Pagamentos == null || Pagamentos.Count != 2)
+                AddNotification("Pagamentos", "Lista de pagamentos deve conter 2 elementos.");
+            else
+            {
+                var somaPagamentos = Pagamentos.Sum(p => p.Valor);
+                var diferencaEntreValorESomaPagamentos = Valor - somaPagamentos;
+
+                if (Math.Abs(diferencaEntreValorESomaPagamentos) > 0.009M)
+                    AddNotification("Pagamentos", "Soma dos pagamentos deve ser igual ao valor da despesa.");
+            }
+        }
+    }
+}
