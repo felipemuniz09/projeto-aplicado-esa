93a1cab44bf7d70f00870a97f61c4295949ae880
felipemuniz09 <felipemuniz09@gmail.com>
2022-09-29 22:10:02 -0300

    PagamentoDespesaValueObject e testes de unidade

---
+++ FinancasParaCasais/FinancasParaCasais.Domain.Test/ValueObject/PagamentoDespesaValueObjectTest.cs
@@ -0,0 +1,28 @@
+﻿using FinancasParaCasais.Domain.ValueObject;
+using FluentAssertions;
+
+namespace FinancasParaCasais.Domain.Test.ValueObject
+{
+    public class PagamentoDespesaValueObjectTest
+    {
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoCodigoDoConjugeNaoForInformado()
+        {
+            // When
+            var despesaConjugeValueObject = new PagamentoDespesaValueObject(Guid.Empty, 7);
+
+            // Then
+            despesaConjugeValueObject.Notifications.Should().Contain(n => n.Message == "Código do cônjuge deve ser informado.");
+        }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoValorForNegativo()
+        {
+            // When
+            var despesaConjugeValueOject = new PagamentoDespesaValueObject(Guid.NewGuid(), -0.01M);
+
+            // Then
+            despesaConjugeValueOject.Notifications.Should().Contain(n => n.Message == "Valor deve ser maior ou igual a zero.");
+        }
+    }
+}
---
+++ FinancasParaCasais/FinancasParaCasais.Domain/ValueObject/PagamentoDespesaValueObject.cs
@@ -0,0 +1,21 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace FinancasParaCasais.Domain.ValueObject
+{
+    public class PagamentoDespesaValueObject : Notifiable<Notification>
+    {
+        public Guid CodigoConjuge { get; private set; }
+        public decimal Valor { get; private set; }
+
+        public PagamentoDespesaValueObject(Guid codigoConjuge, decimal valor)
+        {
+            CodigoConjuge = codigoConjuge;
+            Valor = valor;
+
+            AddNotifications(new Contract<PagamentoDespesaValueObject>()
+                .IsNotEmpty(CodigoConjuge, "CodigoConjuge", "Código do cônjuge deve ser informado.")
+                .IsGreaterOrEqualsThan(Valor, 0, "Valor", "Valor deve ser maior ou igual a zero."));
+        }
+    }
+}
