f34707c6e2c5fc33c0beb28d7ca62e55625501ff
felipemuniz09 <felipemuniz09@gmail.com>
2022-10-01 20:01:18 -0300

    inclusão dos cabeçalhos dos testes

---
+++ FinancasParaCasais/FinancasParaCasais.Application.Test/AppServices/DespesaAppServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using FinancasParaCasais.Application.AppServices;
+using FinancasParaCasais.Domain.Interfaces.Repositories;
+using FinancasParaCasais.Domain.Interfaces.Services;
+using Moq;
+
+namespace FinancasParaCasais.Application.Test.AppServices
+{
+    public class DespesaAppServiceTest
+    {
+        private readonly DespesaAppService _despesaAppService;
+        private readonly Mock<IDespesaService> _despesaServiceMock;
+        private readonly Mock<IDespesaRepository> _despesaRepositoryMock;
+
+        public DespesaAppServiceTest()
+        {
+            _despesaServiceMock = new Mock<IDespesaService>();
+            _despesaRepositoryMock = new Mock<IDespesaRepository>();
+            _despesaAppService = new DespesaAppService(_despesaServiceMock.Object, _despesaRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void DeveLancarExceptionQuandoParametroForNull()
+        {
+
+        }
+
+        [Fact]
+        public void DeveInserirQuandoDespesaForValida()
+        {
+
+        }
+
+        [Fact]
+        public void NaoDeveInserirQuandoDespesaForInvalida()
+        {
+
+        }
+
+        [Fact]
+        public void DeveChamarExclusaoNoRepository()
+        {
+
+        }
+    }
+}
