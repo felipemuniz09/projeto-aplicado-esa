480561dd148c7ca7c56adb8451d0f934a8e009bc
felipemuniz09 <felipemuniz09@gmail.com>
2022-10-02 13:54:30 -0300

    testes de unidade para DespesaAppService

--- FinancasParaCasais/FinancasParaCasais.Application.Test/AppServices/DespesaAppServiceTest.cs
+++ FinancasParaCasais/FinancasParaCasais.Application.Test/AppServices/DespesaAppServiceTest.cs
@@ -1,4 +1,6 @@
 ﻿using FinancasParaCasais.Application.AppServices;
+using FinancasParaCasais.Application.Commands;
+using FinancasParaCasais.Domain.Entities;
 using FinancasParaCasais.Domain.Interfaces.Repositories;
 using FinancasParaCasais.Domain.Interfaces.Services;
 using Moq;
@@ -19,27 +21,60 @@ namespace FinancasParaCasais.Application.Test.AppServices
         }
 
         [Fact]
-        public void DeveLancarExceptionQuandoParametroForNull()
-        {
-
-        }
-
-        [Fact]
         public void DeveInserirQuandoDespesaForValida()
         {
+            // Given
+            var inserirDespesaCommand = new InserirDespesaCommand
+            {
+                Descricao = "Sofá novo",
+                Valor = 2500,
+                Pagamentos = new List<InserirDespesaCommand.PagamentoDespesaCommand>
+                {
+                    new InserirDespesaCommand.PagamentoDespesaCommand { CodigoConjuge = Guid.NewGuid(), Valor = 1000 },
+                    new InserirDespesaCommand.PagamentoDespesaCommand { CodigoConjuge = Guid.NewGuid(), Valor = 1500 },
+                }
+            };
 
+            // When
+            _despesaAppService.InserirDespesa(inserirDespesaCommand);
+
+            // Then
+            _despesaServiceMock.Verify(d => d.InserirDespesa(It.IsAny<Despesa>()), Times.Once);
         }
 
         [Fact]
         public void NaoDeveInserirQuandoDespesaForInvalida()
         {
+            // Given
+            var inserirDespesaCommand = new InserirDespesaCommand
+            {
+                Descricao = "Sofá novo",
+                Valor = 2500,
+                Pagamentos = new List<InserirDespesaCommand.PagamentoDespesaCommand>
+                {
+                    new InserirDespesaCommand.PagamentoDespesaCommand { CodigoConjuge = Guid.NewGuid(), Valor = 1500 },
+                    new InserirDespesaCommand.PagamentoDespesaCommand { CodigoConjuge = Guid.NewGuid(), Valor = 1500 },
+                }
+            };
 
+            // When
+            _despesaAppService.InserirDespesa(inserirDespesaCommand);
+
+            // Then
+            _despesaServiceMock.Verify(d => d.InserirDespesa(It.IsAny<Despesa>()), Times.Never);
         }
 
         [Fact]
         public void DeveChamarExclusaoNoRepository()
         {
+            // Given
+            var codigoDespesa = Guid.NewGuid();
 
+            // When
+            _despesaAppService.ExcluirDespesa(codigoDespesa);
+
+            // Then
+            _despesaRepositoryMock.Verify(d => d.ExcluirDespesa(codigoDespesa), Times.Once);
         }
     }
 }
