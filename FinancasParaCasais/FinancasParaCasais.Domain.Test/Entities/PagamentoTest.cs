2446ffed8322c35b5c764b65a1aecc359bbad097
felipemuniz09 <felipemuniz09@gmail.com>
2022-10-10 20:48:06 -0300

    implementação da entidade Pagamento

---
+++ FinancasParaCasais/FinancasParaCasais.Domain.Test/Entities/PagamentoTest.cs
@@ -0,0 +1,54 @@
+﻿using FinancasParaCasais.Domain.Entities;
+using FluentAssertions;
+
+namespace FinancasParaCasais.Domain.Test.Entities
+{
+    public class PagamentoTest
+    {
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoCodigoConjugePagouForVazio()
+        {
+            // When
+            var pagamento = new Pagamento(Guid.Empty, Guid.NewGuid(), 10);
+
+            // Then
+            pagamento.Notifications.Should().Contain(n => n.Message == "Código do cônjuge que pagou deve ser informado.");
+        }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoCodigoConjugeRecebeuForVazio()
+        {
+            // When
+            var pagamento = new Pagamento(Guid.NewGuid(), Guid.Empty, 10);
+
+            // Then
+            pagamento.Notifications.Should().Contain(n => n.Message == "Código do cônjuge que recebeu deve ser informado.");
+        }
+
+        [Fact]
+        public void DeveConsiderarInvalidoQuandoCodigosDosConjugesForemIguais()
+        {
+            // Given
+            var codigoConjuge = Guid.NewGuid();
+
+            // When
+            var pagamento = new Pagamento(codigoConjuge, codigoConjuge, 10);
+
+            // Then
+            pagamento.Notifications.Should().Contain(
+                n => n.Message == "Código do cônjuge que pagou deve ser diferente do código do cônjuge que recebeu.");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-0.01)]
+        public void DeveConsiderarInvalidoQuandoValorForNegativoOuZero(decimal valor)
+        {
+            // When
+            var pagamento = new Pagamento(Guid.NewGuid(), Guid.NewGuid(), valor);
+
+            // Then
+            pagamento.Notifications.Should().Contain(n => n.Message == "Valor deve ser maior que zero.");
+        }
+    }
+}
---
+++ FinancasParaCasais/FinancasParaCasais.Domain/Entities/Pagamento.cs
@@ -0,0 +1,27 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace FinancasParaCasais.Domain.Entities
+{
+    public class Pagamento : Notifiable<Notification>
+    {
+        public Guid CodigoConjugePagou { get; private set; }
+        public Guid CodigoConjugeRecebeu { get; private set; }
+        public decimal Valor { get; private set; }
+
+        public Pagamento(Guid codigoConjugePagou, Guid codigoConjugeRecebeu, decimal valor)
+        {
+            CodigoConjugePagou = codigoConjugePagou;
+            CodigoConjugeRecebeu = codigoConjugeRecebeu;
+            Valor = valor;
+
+            const string mensagemCodigosIguais = "Código do cônjuge que pagou deve ser diferente do código do cônjuge que recebeu.";
+
+            AddNotifications(new Contract<Pagamento>()
+                .IsNotEmpty(CodigoConjugePagou, "CodigoConjugePagou", "Código do cônjuge que pagou deve ser informado.")
+                .IsNotEmpty(CodigoConjugeRecebeu, "CodigoConjugeRecebeu", "Código do cônjuge que recebeu deve ser informado.")
+                .AreNotEquals(CodigoConjugePagou, CodigoConjugeRecebeu, "CodigoConjugePagou", mensagemCodigosIguais)
+                .IsGreaterThan(Valor, 0, "Valor", "Valor deve ser maior que zero."));
+        }
+    }
+}
