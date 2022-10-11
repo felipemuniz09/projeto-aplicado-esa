f4b49191bffef786e9345c442e92a5c873c831bc
felipe.muniz <felipe.muniz@animaeducacao.com.br>
2022-10-11 15:02:01 -0300

    implementação do PagamentoRepository

---
+++ FinancasParaCasais/FinancasParaCasais.Repository/Repositories/PagamentoRepository.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FinancasParaCasais.Domain.Entities;
+using FinancasParaCasais.Domain.Interfaces.Repositories;
+using FinancasParaCasais.Repository.Entities;
+
+namespace FinancasParaCasais.Repository.Repositories
+{
+    public class PagamentoRepository : BaseRepository, IPagamentoRepository
+    {
+        public PagamentoRepository(FinancasParaCasaisContext context, IMapper mapper) 
+            : base(context, mapper) { }
+
+        public void InserirPagamento(Pagamento pagamento)
+        {
+            var pagamentoEF = _mapper.Map<PagamentoEF>(pagamento);
+
+            _context.Pagamentos.Add(pagamentoEF);
+
+            _context.SaveChanges();
+        }
+    }
+}
